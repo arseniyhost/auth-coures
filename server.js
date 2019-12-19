309ef152f80af2df3c5a7231446f0742bd16ff68
Arseniy <arsen.pilipenko2014@gmail.com>
2019-12-19 22:17:18 +0200

    Version 1.0.2

--- server.js
+++ server.js
@@ -28,10 +28,10 @@ app.use('/api/auth', require('./routes/api/auth'));
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('client/my-app/build'));
+  app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client/my-app', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
